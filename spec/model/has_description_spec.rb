96dae042d19c8bd3ba2a1e2fb26876891e745aba
Postmodern <postmodern.mod3@gmail.com>
2012-11-26 01:50:36 -0800

    Removed duplicate specs.

--- spec/model/has_description_spec.rb
+++ spec/model/has_description_spec.rb
@@ -16,40 +16,6 @@ describe Model::HasDescription do
     subject.properties.should be_named(:description)
   end
 
-  describe "#description" do
-    let(:resource) { DescribedModel.new }
-
-    it "should allow the setting of the description" do
-      resource.description = 'test one'
-      resource.description.should == 'test one'
-    end
-
-    it "should strip leading and tailing white-space" do
-      resource.description = %{   test two    }
-
-      resource.description.should == 'test two'
-    end
-
-    it "should strip leading and tailing white-space from each line" do
-      resource.description = %{
-        test
-        three
-      }
-
-      resource.description.should == "test#{$/}three"
-    end
-
-    it "should preserve non-bordering empty lines" do
-      resource.description = %{
-        test
-
-        four
-      }
-
-      resource.description.should == "test#{$/}#{$/}four"
-    end
-  end
-
   it "should be able to find resources with similar descriptions" do
     subject.create!(:description => 'foo one')
     subject.create!(:description => 'foo bar two')
